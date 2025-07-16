a489425c6872164aa7d20b38c2537c30d2affd04
Ellensramalho <ellen_santos_bsf@hotmail.com>
2025-07-16 20:24:24 -0300

    Mercado Livre Testes

--- cypress/e2e/testes.cy.js
+++ cypress/e2e/testes.cy.js
@@ -1,12 +1,33 @@
-describe('template spec', () => {
+describe('Testes no site do Mercado Livre', () => {
   it('passes', () => {
-    cy.visit('https://www.mercadolivre.com.br/')    
-    cy.get('input[id="cb1-edit"]').type('livros{enter}')
+    cy.visit('https://www.mercadolivre.com.br/');
+    cy.contains('button', 'Mais tarde', { timeout: 10000 })
+      .should('be.visible')
+      .click();
+
+    cy.get('input[id="cb1-edit"]').type('livros{enter}');
+
   
-  cy.origin('https://lista.mercadolivre.com.br', () => {
-      cy.get('#shipping_highlighted_fulfillment').check({ force: true });
-      cy.get('a.poly-component__title').eq(0).click();
-      cy.get('span.andes-button__content').click();
-    })
-  })
-})
+    cy.origin('https://lista.mercadolivre.com.br', () => {
+      cy.get('#shipping_highlighted_fulfillment', { timeout: 20000 })
+        .should('be.visible')
+        .check({ force: true });
+
+        cy.get('input[data-testid="Minimum-price"]').type('20');
+
+
+       cy.get('input[data-testid="Maximum-price"]').type('100');
+
+        cy.get('input[data-testid="Maximum-price"]').type('{enter}');
+
+     
+        cy.get('a.poly-component__title', { timeout: 10000 })
+        .should('have.length.greaterThan', 0)
+        .eq(0)
+        .click();
+    });
+
+  
+  });
+});
+
